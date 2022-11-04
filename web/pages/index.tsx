47576147feafac0cd6cc88843cd19b26b6e011ac
kingban00 <yann.gabriel1@hotmail.com>
2022-11-04 20:43:38 -0300

    configurações iniciais web

---
+++ web/pages/index.tsx
@@ -0,0 +1,28 @@
+interface HomeProps{
+  count: number;
+}
+
+export default function Home(props: HomeProps) {
+  // fetch('http://localhost:3333/pools/count').then(response => response.json())
+  // .then(data => {
+  //   console.log(data)
+  // })
+
+  return (
+   <h1>Contagem: {props.count}</h1>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json()
+
+  // console.log(data)
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
+
+}
